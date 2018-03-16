b39f082f880b066eaa3ca13f0afebed0e6923ea7
Maria1395 <36188509+Maria1395@users.noreply.github.com>
2018-03-16 11:47:22 +0530

    Create EchoServer.java

---
+++ EchoServer.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoServer
+{
+	public EchoServer(int portnum)
+	{
+		try
+		{
+			server = new ServerSocket(portnum);
+		}
+		catch (Exception err)
+		{
+			System.out.println(err);
+		}
+	}
+
+	public void serve()
+	{
+		try
+		{
