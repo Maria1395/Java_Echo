f0b6131051d5b950fb29fc426809f8b166318fdb
Anjali <anjalitbaiju@gmail.com>
2018-03-20 15:50:13 +0530

    Server try-catch updated

--- EchoServer.java
+++ EchoServer.java
@@ -5,6 +5,14 @@ public class EchoServer
 {
 	public EchoServer(int portnum)
 	{
+	    try
+		{
+			server = new ServerSocket(portnum);
+		}
+		catch (Exception err)
+		{
+			System.out.println(err);
+		}
 	}
 
 	public void serve()
